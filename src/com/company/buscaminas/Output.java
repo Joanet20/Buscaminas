34e1e8c59229d26580b1c43ca2313daf326b46f1
Joan Vallbona <johnyvallbona@gmail.com>
2022-04-04 12:21:24 +0200

    printTablero funciona correctament

--- src/com/company/buscaminas/Output.java
+++ src/com/company/buscaminas/Output.java
@@ -1,5 +1,7 @@
 package com.company.buscaminas;
 
+import java.util.ArrayList;
+
 public class Output {
 
     public static void dificultatNoValida(){
@@ -7,10 +9,18 @@ public class Output {
     }
 
     public void displayTablero(Casella[] casellas, int casellesPerFila){
-        for (int i = 0; i < casellas.length; i++){
-            System.out.println();
-            System.out.print("[" + casellas[i].getPosX() + " " + casellas[i].getPosY() + "]");
 
+        ArrayList<Integer> separa = new ArrayList<>();
+
+        for (int i = 0; i < casellesPerFila; i++){
+            separa.add(casellesPerFila * (i+1));
+        }
+
+        for (int i = 0; i < casellas.length; i++){
+            if (separa.contains(i)){
+                System.out.println();
+            }
+            System.out.print("[" + casellas[i].getPosX() + "," + casellas[i].getPosY() + "]");
         }
     }
 }
--- src/com/company/buscaminas/Tablero.java
+++ src/com/company/buscaminas/Tablero.java
@@ -28,19 +28,23 @@ public class Tablero {
     public Tablero omplirTablero(){
         Tablero tablero = crearTablero();
 
-        for (int i = 0; i < tablero.getNumCasellesHor(); i++){
-            for (int j = 0; j < tablero.getNumCasellesVer(); j++){
-                tablero.getCaselles()[i + j] = new Casella(i, j);
-                //System.out.println(tablero.getCaselles()[i+j].getPosX() + " " + tablero.getCaselles()[i+j].getPosY());
+        for (int k = 0; k < tablero.getCaselles().length; k++){
+            for (int i = 0; i < tablero.getNumCasellesHor(); i++){
+                for (int j = 0; j < tablero.getNumCasellesVer(); j++){
+                    tablero.getCaselles()[k] = new Casella(i, j);
+                    k++;
+                    //System.out.println(tablero.getCaselles()[i+j].getPosX() + " " + tablero.getCaselles()[i+j].getPosY());
+                }
             }
         }
+
         return tablero;
     }
 
     public void printTablero(){
         Output output = new Output();
         Tablero tablero = omplirTablero();
-        System.out.println(tablero.getCaselles().length);
+
         output.displayTablero(tablero.getCaselles(), tablero.getNumCasellesHor());
     }
 
